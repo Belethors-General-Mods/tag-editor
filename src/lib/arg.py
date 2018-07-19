dcae2637c02e0c719723adf73234bed1e4e79124
zakimano <zakimano@gmail.com>
2018-07-19 15:53:42 +0200

    refactor | args on a better place

---
+++ src/lib/arg.py
@@ -0,0 +1,27 @@
+# argument handling, cause it looks silly if left in main.py
+
+import argparse
+
+
+def parse():
+    parser = argparse.ArgumentParser(description="Use the BGM management tool from the command line, Huzzah!")
+
+    parser.add_argument("-f", "--frontend", action="store_true",
+        help="flag for using the frontend")
+
+    parser.add_argument("-g", "--get-tag", nargs=1, default=None,
+        help="prints the tag under the given name or ID")
+
+    parser.add_argument("-l", "--list", action="store_true",
+        help="lists all tags")
+
+    parser.add_argument("-a", "--add", nargs=1,
+        help="creates and adds a tag to the database")
+
+    parser.add_argument("-d", "--delete", nargs="+",
+        help="deletes all the specified tags")
+
+    parser.add_argument("-e", "--edit", nargs=1,
+        help="edits a tag's properties")
+
+    return parser.parse_args()
--- src/main.py
+++ src/main.py
@@ -4,9 +4,9 @@
 
 import traceback
 
-import argparse
-
 from frontend.repl import REPL
+
+import lib.arg as arg
 
 import lib.tag as tag
 
@@ -18,25 +18,7 @@ from zenlog import log
 def main() -> None:
     # Start the program.
     
-    parser = argparse.ArgumentParser(description="Use the BGM management tool from the command line, Huzzah!")
-    
-    parser.add_argument("-f", "--frontend", action="store_true",
-        help="flag for using the frontend")
-    
-    parser.add_argument("-g", "--get-tag", nargs=1, default=None,
-        help="prints the tag under the given name or ID")
-    
-    parser.add_argument("-l", "--list", action="store_true",
-        help="lists all tags")
-    
-    parser.add_argument("-a", "--add", nargs=1,
-        help="creates and adds a tag to the database")
-    
-    parser.add_argument("-d", "--delete", nargs="+",
-        help="deletes all the specified tags")
-    
-    parser.add_argument("-e", "--edit", nargs=1,
-        help="edits a tag's properties")
+    args = arg.parse()
     
     # get_tag(self, tag_name: str) -> RETV_TMP:
     # get_list(self) -> RETV_TMP:
@@ -45,7 +27,6 @@ def main() -> None:
     # tag_delete(self, tlist: Optional[list]) -> RETV_TMP:
     # tag_edit(self, tag_name: str, beth: str, gems: str, ncategory: str, ntag: str, steam: str, name: str) -> RETV_TMP:
     
-    args = parser.parse_args()
     # print args for debug purposes
     print(args)
     
